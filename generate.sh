c2b75af0be8a2a03f1ed2ecd524609aae6d13040
Daniel Micay <danielmicay@gmail.com>
2020-12-02 02:47:30 -0500

    remove RSA 2048 support

--- generate.sh
+++ generate.sh
@@ -2,7 +2,6 @@
 
 set -o errexit -o nounset -o pipefail
 
-openssl genrsa -out rsa2048.key 2048
 openssl genrsa -out rsa3072.key 3072
 openssl genrsa -out rsa4096.key 4096
 openssl ecparam -name secp256r1 -genkey -out secp256r1.key
