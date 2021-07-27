25bc9b16e01c23a4c28b13890558eaeb26bb6d98
Daniel Micay <danielmicay@gmail.com>
2021-07-27 17:25:25 -0400

    drop RSA support

--- generate.sh
+++ generate.sh
@@ -2,7 +2,5 @@
 
 set -o errexit -o nounset -o pipefail
 
-openssl genrsa -out rsa3072.key 3072
-openssl genrsa -out rsa4096.key 4096
 openssl ecparam -name secp256r1 -genkey -out secp256r1.key
 openssl ecparam -name secp384r1 -genkey -out secp384r1.key
--- pins.sh
+++ pins.sh
@@ -3,8 +3,6 @@
 set -o errexit -o nounset -o pipefail
 
 PINS=()
-PINS+=($(openssl rsa -in rsa3072.key -outform der -pubout | openssl dgst -sha256 -binary | openssl enc -base64))
-PINS+=($(openssl rsa -in rsa4096.key -outform der -pubout | openssl dgst -sha256 -binary | openssl enc -base64))
 PINS+=($(openssl ec -in secp256r1.key -outform der -pubout | openssl dgst -sha256 -binary | openssl enc -base64))
 PINS+=($(openssl ec -in secp384r1.key -outform der -pubout | openssl dgst -sha256 -binary | openssl enc -base64))
 
